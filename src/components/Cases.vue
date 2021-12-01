1f500071ffbbd0b69ef17e1335b5c6d548b5200c
bexram <bexram33@mail.ru>
2021-12-01 10:01:11 +0300

    background url

--- src/components/Cases.vue
+++ src/components/Cases.vue
@@ -1,44 +1,45 @@
 <template>
     <div
-        class='w-100 case-item h-100 d-flex justify-content-center align-items-center' 
-        v-on:click="readFile()">
-            <img :src="img" alt="">
-            <h3>{{name}}</h3>
-            <p>{{date}}</p>
-        </div>
+            class='w-100 case-item h-100 d-flex justify-content-center align-items-center'
+            v-on:click="readFile()"
+            v-bind:style="{ backgroundImage: 'url('+img+')', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}"
+    >
+        <p>{{date}}</p>
+    </div>
 </template>
 
 <script>
-export default {
-name: 'Cases',
-props: [
-    'path', 'index', 'img', 'name', 'date'
-],
-    methods: {
-        readFile() {
-            window.open(this.path, '_blank') //to open in new tab
+    export default {
+        name: 'Cases',
+        props: [
+            'path', 'index', 'img', 'name', 'date'
+        ],
+        methods: {
+            readFile() {
+                window.open(this.path, '_blank') //to open in new tab
+            }
         }
     }
-}
 </script>
 
 <style lang='scss'>
-    img{
-        height: 100% !important;
-        width: auto !important;
+    div {
+
     }
-.case-item{
-    z-index: 10;
-    position: relative;
-    &:after{
-        display: block;
-        position: absolute;
-        top:0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        content: '';
-        background: linear-gradient(180.79deg, rgba(2, 2, 2, 0) 0.58%, rgba(2, 2, 2, 0.8) 99.32%);
+
+    .case-item {
+        z-index: 10;
+        position: relative;
+
+        &:after {
+            display: block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            content: '';
+            background: linear-gradient(180.79deg, rgba(2, 2, 2, 0) 0.58%, rgba(2, 2, 2, 0.8) 99.32%);
+        }
     }
-}
 </style>
